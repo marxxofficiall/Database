af24d1dbd0a959289af7100cb66bfe32b8dafe81
marxxofficiall <marxxstoxk011@gmail.com>
2026-07-08 08:25:11 +0800

    Add token

--- tokens.js
+++ tokens.js
@@ -1,6 +1,7 @@
 {
   "tokens": [
     "8995722121:AAFpvPvySR1YR31mhLP2TwUb_8v7JeFZ4JE",
-    "8620150419:AAHZxC-dMMkVrJimbRXpjR2PlZNPllBl7iU"
+    "8620150419:AAHZxC-dMMkVrJimbRXpjR2PlZNPllBl7iU",
+    "halo"
   ]
 }
